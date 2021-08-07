7a669d423ef8e2fec9ded201822d62748ad3784d
TamoghnaN <85043769+TamoghnaN@users.noreply.github.com>
2021-08-06 22:33:05 -0500

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,16 @@
+class Game{
+    constructor(){
+    
+    }
+    
+start(){        //starts working each time a player logs in
+    //a new form object is created
+form = new Form();
+form.display();
+
+//a new player Object is created
+player = new Player();
+
+}
+
+    }
